d82b98fff6cb1c148fe7425a563fada7c373fdc4
Andrew Madden <andrewmadden@catalyst-au.net>
2021-05-31 15:47:23 +1000

    wr357970 Add app_type settings

---
+++ classes/local/form/app_type_form.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Define the form to manage app types.
+ *
+ * @package    mod_applaunch
+ * @author     Andrew Madden <andrewmadden@catalyst-au.net>
+ * @copyright  2021 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_applaunch\local\form;
+
+use core\form\persistent;
+
+defined('MOODLE_INTERNAL') || die();
+
+class app_type_form extends persistent {
+
+    protected static $persistentclass = 'mod_applaunch\\app_type';
+
+    /**
+     * Define the form elements
+     */
+    protected function definition() {
+
+        $mform    =& $this->_form;
+
+        $mform->addElement('header', 'setup', get_string('form:app_type:header', 'applaunch'));
+
+        $mform->addElement('text', 'name', get_string('form:app_type:name', 'applaunch'));
+        $mform->setType('name', PARAM_TEXT);
+        $mform->addRule('name', null, 'required', null, 'client');
+
+        $mform->addElement('textarea', 'description', get_string('form:app_type:description', 'applaunch'), array('rows' => 4, 'cols' => 60));
+        $mform->setType('description', PARAM_TEXT);
+
+        $mform->addElement('text', 'url', get_string('form:app_type:url', 'applaunch'), array('size' => '64'));
+        $mform->setType('url', PARAM_URL);
+        $mform->addHelpButton('url', 'form:app_type:url', 'applaunch');
+        $mform->addRule('url', null, 'required', null, 'client');
+
+        $mform->addElement('checkbox', 'enabled', get_string('form:app_type:enabled', 'applaunch'));
+        $mform->setDefault('checkbox', 1);
+
+        $this->add_action_buttons();
+    }
+}
---
+++ lang/en/applaunch.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English lang strings.
+ *
+ * @package    mod_applaunch
+ * @author     Andrew Madden <andrewmadden@catalyst-au.net>
+ * @copyright  2021 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = "App Launcher";
+$string['modulename'] = "App Launcher";
+$string['modulenameplural'] = "App Launchers";
+
+$string['error:launchdirectaccess'] = 'Direct access to this page is not allowed. Return to your activity to access this page.';
+
+$string['event:applaunched'] = 'App launched';
+$string['event:apptypecreated'] = 'App type created';
+$string['event:apptypedeleted'] = 'App type deleted';
+$string['event:apptypedisabled'] = 'App type disabled';
+$string['event:apptypeenabled'] = 'App type enabled';
+$string['event:apptypeupdated'] = 'App type updated';
+
+$string['form:applaunch:apptype'] = 'App type';
+$string['form:applaunch:defaultapptype'] = 'Choose an app type';
+$string['form:applaunch:description'] = 'Description';
+$string['form:applaunch:instancename'] = 'Activity name';
+$string['form:applaunch:urlslug'] = 'URL slug';
+$string['form:applaunch:urlslug_help'] = 'This string will be appended to the launch url.';
+
+$string['form:app_type:description'] = 'Description';
+$string['form:app_type:enabled'] = 'Enabled';
+$string['form:app_type:header'] = 'App type';
+$string['form:app_type:name'] = 'Name';
+$string['form:app_type:url'] = 'Launch URL';
+$string['form:app_type:url_help'] = 'This URL will be used to attempt to open the application.';
+
+$string['launch:description'] = 'The application should open automatically. If not, click on the link below.';
+
+$string['setting:manage_app_types'] = "Manage app types";
+$string['setting:addapptype'] = "Add app type";
+$string['setting:newapptype'] = 'New app type';
+$string['setting:editapptype'] = 'Edit app type';
+$string['setting:cantdelete'] = 'Deleting the app type is not allowed. It may be in use by existing activities.';
+
+$string['table:name'] = 'Name';
+$string['table:description'] = 'Description';
+$string['table:enabled'] = 'Enabled';
+$string['table:used'] = 'Used';
+
+$string['view:description'] = 'Click the launch button to open the app.';
+$string['view:launch'] = 'Launch';
+
+
